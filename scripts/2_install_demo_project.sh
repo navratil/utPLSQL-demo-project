b6d1201df24dcc2cb5ac4dbc7d4330d726ac18ac
Jan Navratil <jan.navratil2@gmail.com>
2024-01-04 22:45:22 +0000

    Update 2_install_demo_project.sh

--- scripts/2_install_demo_project.sh
+++ scripts/2_install_demo_project.sh
@@ -1,7 +1,7 @@
 docker run --rm -v $(pwd):/work -w /work/  --network host \
-    --entrypoint sqlplus gvenzl/oracle-xe:21-slim \
-    sys/oracle@//127.0.0.1:1521/XE as sysdba @source/create_user.sql
+    --entrypoint sqlplus gvenzl/oracle-free:23-slim \
+    sys/oracle@//127.0.0.1:1521/freepdb1 as sysdba @source/create_user.sql
 
 docker run --rm -v $(pwd):/work -w /work/  --network host \
-    --entrypoint sqlplus gvenzl/oracle-xe:21-slim \
-    ut3_demo/ut3_demo@//127.0.0.1:1521/XE @source/install.sql
+    --entrypoint sqlplus gvenzl/oracle-free:23-slim \
+    ut3_demo/ut3_demo@//127.0.0.1:1521/freepdb1 @source/install.sql
