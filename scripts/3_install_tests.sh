45eca4f254d5dd9987802136d3a9608865277d5b
Jan Navratil <jan.navratil2@gmail.com>
2024-01-04 22:46:47 +0000

    Update 3_install_tests.sh

--- scripts/3_install_tests.sh
+++ scripts/3_install_tests.sh
@@ -1,3 +1,3 @@
 docker run --rm -v $(pwd):/work -w /work/  --network host \
-    --entrypoint sqlplus gvenzl/oracle-xe:21-slim \
-    ut3_demo/ut3_demo@//127.0.0.1:1521/XE @test/install.sql
+    --entrypoint sqlplus gvenzl/oracle-free:23-slim \
+    ut3_demo/ut3_demo@//127.0.0.1:1521/freepdb1 @test/install.sql
